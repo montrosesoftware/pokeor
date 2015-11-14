c70fcc6bad967a9277b4713b20d96cdb02097658
Marek Krzynowek <marek.krzynowek@montrosesoftware.com>
2015-11-14 11:39:39 +0100

    updated database to store single collection of games.

--- collections.js
+++ collections.js
@@ -1,14 +1,15 @@
 Games = new Mongo.Collection('games');
-Players = new Mongo.Collection('players');
 
-Players.helpers({
-  game: function() {
-    return Games.findOne(this.playerId);
-  }
-});
+// Players = new Mongo.Collection('players');
 
-Games.helpers({
-  players: function() {
-    return Players.find({ gameId: this._id });
-  }
-});
+// Players.helpers({
+//   game: function() {
+//     return Games.findOne(this.playerId);
+//   }
+// });
+
+// Games.helpers({
+//   players: function() {
+//     return Players.find({ gameId: this._id });
+//   }
+// });
