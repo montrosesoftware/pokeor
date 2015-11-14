a75919946b794ffdf5d72304396ff6bf21aa3a3a
Marek Krzynowek <marek.krzynowek@montrosesoftware.com>
2015-11-14 20:10:16 +0100

    Cards shown Yay

--- cards.js
+++ cards.js
@@ -7,7 +7,15 @@ if (Meteor.isClient) {
       		var hands =  Games.current().currentRound().hands;
       		var player = Session.get("player");
       		console.log(hands);
-      		console.log("playa: "+ player);
+      		console.log("playa: " + player);
+      		var playerHand = [];
+      		hands.forEach( function(hand){
+      			  if(hand.player === player){
+      			  	playerHand = hand.hand;
+      			  	return;
+      			  }
+      		});
+      		return playerHand;
   		}
     }
   });
