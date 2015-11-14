4141a9fa926505b0cfb0487837a7715a7a335ea4
Michal Fronczyk <fronczyk@gmail.com>
2015-11-14 13:11:26 +0100

    Added init to games.

--- games.js
+++ games.js
@@ -3,6 +3,13 @@ Games = new Mongo.Collection('games');
 Games.helpers({
   currentRound: function() {
     return Rounds.findOne(this.currentRoundId);
+  },
+
+  init: function() {
+    Rounds.insert({
+      gameId: this._id,
+      dealer: this.players[0]
+    });
   }
 });
 
