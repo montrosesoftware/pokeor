20015e248a4ccd4fd4426e78220051ded6108a7e
Marek Krzynowek <marek.krzynowek@montrosesoftware.com>
2015-11-14 11:25:36 +0100

    Added one to many.

---
+++ collections.js
@@ -0,0 +1,14 @@
+Games = new Mongo.Collection('games');
+Players = new Mongo.Collection('players');
+
+Players.helpers({
+  game: function() {
+    return Games.findOne(this.playerId);
+  }
+});
+
+Games.helpers({
+  players: function() {
+    return Players.find({ gameId: this._id });
+  }
+});
